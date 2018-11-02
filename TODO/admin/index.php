57ab6c7e34d82b9c8d4e6569b12eda308aac727d
xiaoq <xiaoq>
2018-11-02 10:05:27 +0800

    添加跳转用的 index.php

---
+++ TODO/admin/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<script>
+		location.href = 'http://todoblock.io:8080/admin'
+	</script>
+</head>
+<body>
+
+</body>
+</html>
